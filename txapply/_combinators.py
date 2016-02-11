7ed0ff3e64b1c764eb3ed244bb0133e9ee3f1784
Jonathan Lange <jml@mumak.net>
2016-02-11 16:26:08 +0000

    Add ``combine``, improve docstrings

--- txapply/_combinators.py
+++ txapply/_combinators.py
@@ -10,7 +10,8 @@ def nop(*args, **kwargs):
 
 
 def transparent(value, function, *args, **kwargs):
-    """Invoke ``function`` with ``value`` and other arguments, return ``value``.
+    """
+    Invoke ``function`` with ``value`` and other arguments, return ``value``.
 
     Use this to add a function to a callback chain without disrupting the
     value of the callback chain::
@@ -24,4 +25,33 @@ def transparent(value, function, *args, **kwargs):
 
 
 def ignore(value, function, *args, **kwargs):
+    """
+    Invoke ``function`` with ``*args`` and ``*kwargs``.
+
+    Use this to add a function to a callback chain that just ignores the
+    previous value in the chain::
+
+       >>> d = defer.succeed(42)
+       >>> d.addCallback(ignore, print, 37)
+       37
+    """
     return function(*args, **kwargs)
+
+
+def combine(value, function, *args, **kwargs):
+    """
+    Call ``function``, return its result and ``value`` as a tuple.
+
+    ``function`` is invoked with ``value``, ``*args`` and ``**kwargs`` and
+    then we return a 2-tuple of whatever ``function`` returned and ``value``.
+
+    Use this to add a function to a callback chain that combines its return
+    value with the previous value::
+
+        >>> d = defer.succeed(42)
+        >>> d.addCallback(combine, lambda: 47)
+        >>> d.addCallback(print)
+        (37, 42)
+    """
+    y = function(value, *args, **kwargs)
+    return (y, value)
--- txapply/tests/test_combinators.py
+++ txapply/tests/test_combinators.py
@@ -8,7 +8,7 @@ from testtools.matchers import Equals, Is
 from testtools.twistedsupport import succeeded
 
 from twisted.internet.defer import succeed
-from .._combinators import ignore, nop, transparent
+from .._combinators import combine, ignore, nop, transparent
 from .strategies import any_value, arguments, keyword_arguments
 
 
@@ -33,7 +33,7 @@ class TestTransparent(TestCase):
     @given(first=any_value(), second=any_value())
     def test_passes_value_through(self, first, second):
         """
-        The return value of a callbacks added with ``transparent`` is ignored,
+        The return value of a callback added with ``transparent`` is ignored,
         and instead the previous value in the chain is passed through.
         """
         d = succeed(first)
@@ -66,6 +66,10 @@ class TestIgnore(TestCase):
     @given(first=any_value(), second=any_value(), args=arguments(),
            kwargs=keyword_arguments())
     def test_ignores_callback_value(self, first, second, *args, **kwargs):
+        """
+        Callbacks added with ``ignore`` don't get passed the return value of
+        the previous callback.
+        """
         log = []
 
         def callback(*a, **kw):
@@ -75,3 +79,28 @@ class TestIgnore(TestCase):
         d.addCallback(ignore, callback, *args, **kwargs)
         self.assertThat(d, succeeded(Is(second)))
         self.assertThat(log, Equals([(args, kwargs)]))
+
+
+class TestCombine(TestCase):
+    """
+    Tests for ``combine``.
+    """
+
+    @given(first=any_value(), second=any_value(), args=arguments(),
+           kwargs=keyword_arguments())
+    def test_combines_results(self, first, second, *args, **kwargs):
+        """
+        The return value of a callback added with ``combine`` is a tuple made
+        up of the *previous* callback value and whatever the new callback
+        returns.
+        """
+        log = []
+
+        def callback(value, *a, **kw):
+            log.append((value, a, kw))
+            return second
+
+        d = succeed(first)
+        d.addCallback(combine, callback, *args, **kwargs)
+        self.assertThat(d, succeeded(Equals((second, first))))
+        self.assertThat(log, Equals([(first, args, kwargs)]))
